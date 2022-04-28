ded2564d847958e1b01e30f9b8f690a8b30cd998
PuhliyMops <kb36@list.ru>
2022-04-28 22:56:50 +0300

    added faq

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,12 +1,43 @@
 $(document).ready(function(){
 
-  let isOpen = false;
-
   $('.j-burger').on('click', function(){
 
     $('.j-menu').slideToggle();
 
 
   })
+
+
+  // Табы в контактах
+
+
+  // Фильтр в наших работах
+
+
+  // Аккордеон
+  let prevFaqBtn;
+
+  $('.j-faq-btn').on('click',function(){
+
+    if(prevFaqBtn === this) {
+      $(this).next().slideToggle();
+
+      return ;
+    }
+
+    $('.j-faq-btn').next().slideUp();
+    $(this).next().slideDown();
+
+    prevFaqBtn = this;
+  });
+
+
+// Отзывы-каруселька
+
+  $('.j-carousel').slick({
+  dots: true
+  });
+
+
 
 })
